8a12cc3c711a84ef60b09110b1be78d36350f08b
René Nyffenegger <mail@renenyffenegger.ch>
2018-07-23 20:21:46 +0200

    Some comments

--- assembler/first-program/first.asm
+++ assembler/first-program/first.asm
@@ -2,6 +2,6 @@ SECTION .text
   global _start
 _start:
 
-mov eax,  1
-mov ebx, 42
-int 0x80
+mov eax,  1 ; syscall number (here: exit)
+mov ebx, 42 ; first (and only) argument to syscall exit
+int 0x80    ; execute syscall
