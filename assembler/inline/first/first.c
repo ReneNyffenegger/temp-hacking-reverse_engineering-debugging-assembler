c9d861c369fd92744011444fc75430a5d537b2a7
René Nyffenegger <mail@renenyffenegger.ch>
2018-07-27 23:10:48 +0200

    2018-07-27

--- assembler/inline/first/first.c
+++ assembler/inline/first/first.c
@@ -5,13 +5,18 @@ int main() {
   int a=21;
   int b= 7;
 
+
   asm volatile (
-      ".intel_syntax noprefix;"
-       "mov  eax,%0;"  // load eax register with a, being the first factor of the multiplication
-       "mov  ebx,2 ;"  // load ebx register with 2, being the second factor of the multiplication
-       "mul  ebx   ;"  // execute multiplication (ebx = ebx * eax)
-       "mov  %1,eax;"  // return ebx to output.
-      ".att_syntax ;"  // Apparently, returning to AT&T syntax is necessary.
+   // The first 'argument' is a string. In order to make the assembler code a bit more
+   // readable, each instruction goes onto one line. However, c (without special extensions)
+   // does not allow multiline strings. With string concatenation, strings are concatenated.
+   // Each assembler instruction is terminaed with a semicolon.
+      ".intel_syntax noprefix;"  // Turn on intel dialect.
+       "mov  eax,%0          ;"  // load eax register with a, being the first factor of the multiplication
+       "mov  ebx,2           ;"  // load ebx register with 2, being the second factor of the multiplication
+       "mul  ebx             ;"  // execute multiplication (ebx = ebx * eax)
+       "mov  %1,eax          ;"  // return ebx to output.
+      ".att_syntax           ;"  // Apparently, returning to AT&T syntax is necessary.
     // Output opperands (seperated by a colon)
      : "=r"(b)
     // Input opperands (seperated by a colon)
