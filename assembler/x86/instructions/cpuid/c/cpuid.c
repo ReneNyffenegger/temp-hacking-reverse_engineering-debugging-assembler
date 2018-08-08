f8065ed5861fbd92a2e8968b5e486bf1218262b2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-08-08 13:07:33 +0200

    + cpuid

--- assembler/x86/instructions/cpuid/c/cpuid.c
+++ assembler/x86/instructions/cpuid/c/cpuid.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 //
 // CPUID can be executed if bit 21 (ID flag) in the EFLAGS register can be cleared and set.
@@ -30,19 +31,30 @@ int main(int argc, char **argv) {
   static char cpu_vendor[13];
 
   unsigned int func_no = 0;
+  unsigned int eax, ebx, ecx, edx    ;
+               eax= ebx= ecx= edx = 0;
 
   asm volatile ( // Compare https://stackoverflow.com/a/19412944/180275
 //  ".intel_syntax noprefix\n"
-    "mov  eax, %1          \n"
+//  "mov  eax, %1          \n"
+    "mov  eax, %4          \n"
     "cpuid                 \n"
-    "mov [%0+0], ebx       \n"
-    "mov [%0+4], edx       \n"
-    "mov [%0+8], ecx       \n"
+//  "mov [%0+0], ebx       \n"
+//  "mov [%0+4], edx       \n"
+//  "mov [%0+8], ecx       \n"
+    "mov  %0   , ebx       \n"
+    "mov  %1   , ebx       \n"
+    "mov  %2   , edx       \n"
+    "mov  %3   , ecx       \n"
 //  ".att_syntax           \n"
-    :"=m"(cpu_vendor)
+    :"=m"(eax), "=m"(ebx), "=m"(ecx),"=m"(edx)
     :"m"(func_no)
     :"eax", "ebx", "edx", "ecx"
   );
+
+  memcpy((void*) cpu_vendor  , (void*) &ebx, 4);
+  memcpy((void*) cpu_vendor+4, (void*) &edx, 4);
+  memcpy((void*) cpu_vendor+8, (void*) &ecx, 4);
 
   printf("Vendorname: %s\n", cpu_vendor);
 
