d42c2663ea7a84a3a777a668176ecdb892d949e9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-08-08 14:38:19 +0200

    n/a

--- assembler/x86/instructions/cpuid/c/cpuid.c
+++ assembler/x86/instructions/cpuid/c/cpuid.c
@@ -5,58 +5,78 @@
 // CPUID can be executed if bit 21 (ID flag) in the EFLAGS register can be cleared and set.
 //
 
-// int main() {
-// 
-//   unsigned int ax, ebx, ecx, edx    ;
-//                ax= ebx= ecx= edx = 0;
-// 
-// 
-//   asm volatile (
-//     ".intel_syntax noprefix;"
-//     "cpuid;"
-//     "mov %1, eax;"
-// //  "mov %2, ebx;"
-// //  "mov %3, ecx;"
-// //  "mov %4, edx;"
-//     ".att_syntax;"
-//     : "=m" (eax)
-//     :
-//     : "eax", "ebx", "ecx", "edx"
-//   );
-// 
-// }
 
+unsigned int eax, ebx, ecx, edx;
+int maxFuncNr = 0;
 
-int main(int argc, char **argv) {
-  static char cpu_vendor[13];
+void cpuid(unsigned int func_no) {
 
-  unsigned int func_no = 0;
-  unsigned int eax, ebx, ecx, edx    ;
-               eax= ebx= ecx= edx = 0;
+  if (func_no > maxFuncNr) return;
 
-  asm volatile ( // Compare https://stackoverflow.com/a/19412944/180275
-//  ".intel_syntax noprefix\n"
-//  "mov  eax, %1          \n"
+  asm volatile (
+  //"----------------------\n"
     "mov  eax, %4          \n"
     "cpuid                 \n"
-//  "mov [%0+0], ebx       \n"
-//  "mov [%0+4], edx       \n"
-//  "mov [%0+8], ecx       \n"
-    "mov  %0   , ebx       \n"
+    "mov  %0   , eax       \n"
     "mov  %1   , ebx       \n"
-    "mov  %2   , edx       \n"
-    "mov  %3   , ecx       \n"
-//  ".att_syntax           \n"
+    "mov  %2   , ecx       \n"
+    "mov  %3   , edx       \n"
+  //"----------------------\n"
     :"=m"(eax), "=m"(ebx), "=m"(ecx),"=m"(edx)
     :"m"(func_no)
     :"eax", "ebx", "edx", "ecx"
   );
 
-  memcpy((void*) cpu_vendor  , (void*) &ebx, 4);
-  memcpy((void*) cpu_vendor+4, (void*) &edx, 4);
-  memcpy((void*) cpu_vendor+8, (void*) &ecx, 4);
+//printf("Func no: %d: %8x %8x %8x %8x\n", func_no, eax, ebx, ecx, edx);
 
-  printf("Vendorname: %s\n", cpu_vendor);
+}
+
+inline unsigned int extractBits(unsigned int val, int start, int endIncl) {
+
+  unsigned int bits = (1 << (endIncl-start+1)) - 1;
+//printf("bits: %d\n", bits);
+
+  return  (val & (bits << start) ) >> start;
+
+//return (val & (( (1 << (end-start)) - 1) >> start) ) >> start;
+
+}
+
+int main(int argc, char **argv) {
+  static char cpu_vendor[13];
+
+  printf("%d\n", extractBits(255, 3, 4));
+
+
+
+  cpuid(0);
+
+    maxFuncNr= eax;
+
+    printf("Max func nr: %d\n", maxFuncNr);
+
+    memcpy((void*) cpu_vendor  , (void*) &ebx, 4); // Note the order of the
+    memcpy((void*) cpu_vendor+4, (void*) &edx, 4); // registers: ebx, edx, ecx
+    memcpy((void*) cpu_vendor+8, (void*) &ecx, 4); //
+  
+    printf("Vendorname : %s\n", cpu_vendor);
+
+  cpuid(1);
+    printf("stepping id:     %d\n", extractBits(eax,  0,  3));
+    printf("Model Nr:        %d\n", extractBits(eax,  4,  7));
+    printf("Family code:     %d\n", extractBits(eax,  8, 11));
+    printf("Processor type:  %d\n", extractBits(eax, 12, 13));
+    printf("Extended model:  %d\n", extractBits(eax, 16, 19));
+    printf("Extended family: %d\n", extractBits(eax, 20, 27));
+
+    printf("\n");
+
+    printf("TSC             : %d\n", extractBits(edx,  4, 4)); /* Time stamp counter */
+    printf("sysenter/sysexit: %d\n", extractBits(edx, 11,11));
+    printf("Thermal monitor : %d\n", extractBits(edx, 22,22));
+
+    printf("AES             : %d\n", extractBits(ecx, 25,25));
 
   return 0;
 }
+
