8bd963d63bbbbe77832cab5be49124f69306068d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-03-09 21:01:44 +0100

    + func.asm

--- windows/PE-COFF-Format/first/compile.ps1
+++ windows/PE-COFF-Format/first/compile.ps1
@@ -1,8 +1,13 @@
 # cl /Fa /c entry.c
 
-  ml64 /c entry.asm
+  ml64 /nologo /c entry.asm
 
 # cl /Fa /c func.c
+
+  ml64 /nologo /c func.asm
+
 # cl /Fa /c consts.c
+
+  ml64 /nologo /c consts.asm
 
 link /nologo /entry:entry /nodefaultlib /subsystem:windows /machine:x64 entry.obj func.obj consts.obj kernel32.lib user32.lib /out:prog.exe
