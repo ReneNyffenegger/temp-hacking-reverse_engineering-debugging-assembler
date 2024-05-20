8657089f91238bceadb66a52f421f78a0b7a9761
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-05-20 18:23:55 +0200

    .

---
+++ assembler/patterns/write-string/write-string.s
@@ -0,0 +1,18 @@
+.section .data
+    hello_world: .asciz "Hello, World!\n"
+
+.section .text
+    .global _start
+
+_start:
+  # Write the string to stdout
+    mov $1 , %rax            # syscall: write
+    mov $1 , %rdi            # file descriptor: stdout
+    lea hello_world(%rip), %rsi # address of the string
+    mov $14 , %rdx           # number of bytes to write
+    syscall
+
+  # Exit the program
+    mov $60, %rax           # syscall: exit
+    xor %rdi, %rdi          # exit status: 0
+    syscall
