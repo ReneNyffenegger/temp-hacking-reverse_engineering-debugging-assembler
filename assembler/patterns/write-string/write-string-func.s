c979e60ff22f0756f0394a3d2a1be2eb2e7c3cf6
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-05-20 20:33:54 +0200

    .

--- assembler/patterns/write-string/write-string-func.s
+++ assembler/patterns/write-string/write-string-func.s
@@ -1,5 +1,6 @@
  .section .data
-    hello_world: .asciz "Hello, World!\n"
+    hello_world:    .asciz "Hello, World!\n"
+    another_string: .asciz "Another string\n"
 
 .section .bss
     len: .skip 8  # reserve space for the length
@@ -9,12 +10,12 @@
 
 
 _start:
+
   # Call strlen to get the length of the string in hello_world
     lea hello_world(%rip), %rsi  # Load the address of the string into rsi
 
   # --- Determine length of string pointed at in rsi) --------------
   
-#       xor %rax, %rax  # Clear rax (will hold the length)
         xor %rcx, %rcx  # Clear rcx (counter)
      
        .loop:
@@ -39,6 +40,32 @@ _start:
     syscall
 
   # ----------------------------------------------------------------
+
+
+    lea another_string(%rip), %rsi  # Load the address of the string into rsi
+
+        xor %rcx, %rcx  # Clear rcx (counter)
+     
+       .loop1:
+           movb (%rsi,%rcx), %dl  # Load byte from rsi + rcx into dl
+           test %dl, %dl          # Test if the byte is zero
+           je .done1              # If zero, end of string
+           inc %rcx               # Increment counter
+           jmp .loop1             # Repeat loop
+     
+        .done1:
+            movq %rcx, %rdx
+
+  # ----------------------------------------------------------------
+
+  #
+  # write string
+  #
+    mov $1, %rax                 # syscall nr: 1 = syscall nr for write
+    mov $1, %rdi                 # param 1   : 1 = file descriptor for stdout
+                                 # param 2   : address of string (already stored in %rdi)
+                                 # param 3   : len of string     (already stored in %rdx)
+    syscall
 
   # Exit the program
 
