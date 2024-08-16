c9c4d0918de506e9b9acab082435009256e09c82
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-08-16 20:25:52 +0200

    .

---
+++ gdb/commands/step-next/multiple-statements-in-one-line/next.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+
+    int a;
+    int b;
+    int c;
+
+    a = 5;
+    b = 7; c = 9; printf("a = %d, b = %d, c = %d\n", a, b, c);
+
+}
---
+++ gdb/commands/step-next/multiple-statements-in-one-line/step.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int a;
+int b;
+int c;
+int c;
+
+void f() {
+
+    b = 7; c = 9; printf("a = %d, b = %d, c = %d\n", a, b, c);
+}
+
+int main() {
+
+    a = 5; b = 6;
+    f();
+
+}
