b3a77b959f9b91d57d711297bab269ca51a8fffb
René Nyffenegger <mail@renenyffenegger.ch>
2024-09-13 00:20:59 +0200

    .

---
+++ linux/_start/arguments/_start.s
@@ -0,0 +1,14 @@
+.intel_syntax noprefix
+
+.file "_start.s"
+
+#.section .text
+  .text
+  .globl _start
+  .type  _start, @function
+
+_start:
+
+mov rax,   60
+mov rdi,   17
+syscall
---
+++ linux/_start/arguments/readme.txt
@@ -0,0 +1,3 @@
+See also
+  /home/rene/github/dev/hacking-reverse_engineering-debugging-assembler/temp/elf/entry-point-address
+
