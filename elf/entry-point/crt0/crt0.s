124519d5dcecbe23e29ddd0fa293a9539f06d43a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-09-15 18:28:29 +0200

    .

--- elf/entry-point/crt0/crt0.s
+++ elf/entry-point/crt0/crt0.s
@@ -1,16 +1,22 @@
-	.text
-	.globl	_start
+        .text
+        .globl  _start
 _start:
-	leaq	8(%rsp),%rsi	# argv
-	movq	0(%rsp),%rcx	# argc
-	movq	%rcx,%rax	# environ = &argv[argc+1]
-	incq	%rax
-	shlq	$3,%rax
-	addq	%rsi,%rax
-	movq	%rax,Cenviron
-	pushq	%rsi
-	pushq	%rcx
-	call	Cmain
-	addq	$16,%rsp
-	pushq	%rax
-  ret
+        leaq    8(%rsp),%rsi    # argv
+        movq    0(%rsp),%rcx    # argc
+        movq    %rcx,%rax       # environ = &argv[argc+1]
+        incq    %rax            # rax = rax + 1
+        shlq    $3,%rax         # rax = rax * 8
+        addq    %rsi,%rax       # rax = rax + rsi
+#       movq    %rax,Cenviron
+        pushq   %rsi
+        pushq   %rcx
+#       call    Cmain
+
+        call    main
+
+        movq    %rax , %rdi
+        movq    $60  , %rax
+        syscall
+#       addq    $16,%rsp
+#       pushq   %rax
+# ret
