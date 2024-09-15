dfdadaf19d5ebe90db063ece9b74e503e08bff77
René Nyffenegger <mail@renenyffenegger.ch>
2024-09-15 22:40:37 +0200

    .

--- elf/entry-point/crt0/crt0.s
+++ elf/entry-point/crt0/crt0.s
@@ -1,16 +1,23 @@
         .text
         .globl  _start
 _start:
-        leaq    8(%rsp),%rsi    # argv
-        movq    0(%rsp),%rcx    # argc
-        movq    %rcx,%rax       # environ = &argv[argc+1]
-        incq    %rax            # rax = rax + 1
-        shlq    $3,%rax         # rax = rax * 8
-        addq    %rsi,%rax       # rax = rax + rsi
-#       movq    %rax,Cenviron
-        pushq   %rsi
-        pushq   %rcx
-#       call    Cmain
+
+    pop           %rdi
+    leaq  (%rsp), %rsi
+
+# #       movq    0(%rsp),%rdi    # argc
+#         movq     (%rsp),%rdi    # argc
+# #       leaq    0(%rsp),%rdi    # argc
+#         leaq    8(%rsp),%rsi    # argv
+
+#         movq    %rdi,%rax       # environ = &argv[argc+1]
+#         incq    %rax            # rax = rax + 1
+#         shlq    $3,%rax         # rax = rax * 8
+#         addq    %rsi,%rax       # rax = rax + rsi
+# #       movq    %rax,Cenviron
+# #       pushq   %rsi
+# #       pushq   %rdi
+# #       call    Cmain
 
         call    main
 
--- elf/entry-point/crt0/prog.c
+++ elf/entry-point/crt0/prog.c
@@ -1,8 +1,11 @@
-void printstring(char*);
+void writestring(char*);
 
 int main(int argc, char* argv[]) {
 
-    printstring(argv[0]);
+    for (int i=0; i<argc; i++) {
+       writestring(argv[i]);
+       writestring("\n");
+    }
 
 //  size_t len = 0;
 //  while (argv[0][len] != '\0') {  // find length of string
